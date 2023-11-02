9eecde07b2b6a91022831f09912e752d9f3ab281
abhishek Hegde <ahegde2021@gmail.com>
2023-11-02 19:16:48 +0530

    sign - in done
    fixed problem of not rendering sig-in due to storage cashe.
    checked if user is saved in database in appwrite

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -69,8 +69,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const cookieFallback = localStorage.getItem("cookieFallback");
     if (
-      cookieFallback === "[]" 
-      // cookieFallback === null ||
+      cookieFallback === "[]" ||
+      cookieFallback === null 
       // cookieFallback === undefined
     ) {
       navigate("/sign-in");
