7403ed63b38ea64ef7d5cf3594d340d9482ebfd3
abhishek Hegde <ahegde2021@gmail.com>
2023-11-02 17:47:25 +0530

    sign up for - redirecting to home page after successful submit

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {ReactNode} from 'react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient();
 
-export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+export const QueryProvider = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
-};
+  )
+}
